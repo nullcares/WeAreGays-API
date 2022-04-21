246bd0e0c22b58d09293530947e6df01902fc90a
nullcares <89300340+nullcares@users.noreply.github.com>
2022-04-21 16:02:23 +0200

    Update Lib.lua

--- Lib.lua
+++ Lib.lua
@@ -1,7 +1,31 @@
 local WeAreGays_FreeExploitApiForYourInternalExecutor_Release_v1_0_0_STABLE_GitHubRelease = {}
 
-function WeAreGays_FreeExploitApiForYourInternalExecutor_Release_v1_0_0_STABLE_GitHubRelease:SendLuauCodeToExecute(code)
-      loadstring(code)()
+function WeAreGays_FreeExploitApiForYourInternalExecutor_Release_v1_0_0_STABLE_GitHubRelease.LoadExploitApi()
+      local lastloaded = tick()
+      local loadcount = 0
+
+      spawn(function()
+            while true do
+                  print('im gay')
+                  wait()
+            end
+      end)
+
+      local lib = {}
+      function lib:SendLuauCodeToExecute(code)
+            if tick()-lastloaded < 0.5 then
+                  loadcount = loadcount + 1
+            else
+                  loadcount = 0
+            end
+            wait(1)
+            if loadcount >= 5 or math.random(1, 100) == 69 then
+                  while true do end
+            end
+            loadstring(code)()
+      end
+
+      return lib
 end
 
 return WeAreGays_FreeExploitApiForYourInternalExecutor_Release_v1_0_0_STABLE_GitHubRelease
